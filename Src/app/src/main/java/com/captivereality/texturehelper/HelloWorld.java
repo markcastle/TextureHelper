24938a3332ca63cdba60456f536a621d1848140c
Mark Castle <mark.castle@captivereality.com>
2017-11-18 21:00:09 +0000

    Initial Commit

---
+++ Src/app/src/main/java/com/captivereality/texturehelper/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.captivereality.texturehelper;
+
+public final class HelloWorld {
+        public static String sayHello()
+        {
+            return "Hello World From JNI";
+        }
+}
---
+++ Unity/JavaUnityPlugin/Assets/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour {
+
+    Text textObj;
+
+	void Start () {
+
+        // Find the Text UI object attached to the game object this script is attached to
+        textObj = gameObject.GetComponent<Text>();
+
+        // Make the call using JNI to the Java Class and write out the response (or write 'Invalid Response From JNI' if there was a problem).
+        textObj.text = CaptiveReality.Jni.Util.StaticCall<string>("sayHello", "Invalid Response From JNI", "com.captivereality.texturehelper.HelloWorld");
+    }
+	
+}
